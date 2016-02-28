3b92830afd13466bf9cfe6a9d3f230d398a99ddd
MHONIELSEN <mhn.nospam@gmail.com>
2016-02-28 20:00:54 +0100

    fixes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get "contact" => "pages#kontakt"
-  get "webshop" => "pages#webshop"
+  get "webshop" => "items#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
